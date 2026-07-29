60afde419226a1ab8fed9bf9a5dc10397014678e
Star Natural <www.starnatural.co@gmail.com>
2026-07-29 14:57:42 -0500

    Update app.js
    v1.1.3

--- js/app.js
+++ js/app.js
@@ -117,10 +117,8 @@ function renderProducts() {
           <button class="btn-add-cart" onclick="addToCart('${product.id}')">+ Agregar al Carrito</button>
         </div>
 <div class="product-footer" style="margin-top: 0.8rem;">
-  <button class="btn-add-cart" onclick="addToCart('${product.id}')" style="display: inline-flex; align-items: center; justify-content: center; gap: 6px;">
-    <img src="${EMOJIS.cart}" class="animated-emoji" alt="Carrito"> Agregar al Carrito
-  </button>
-</div>
+          <button class="btn-add-cart" onclick="addToCart('${product.id}')">+ Agregar al Carrito</button>
+        </div>
 </div>
       </div>
     `;
