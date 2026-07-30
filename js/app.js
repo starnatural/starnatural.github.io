03c02bd1d9c05dc7692032f13baaa7988045a899
Star Natural <www.starnatural.co@gmail.com>
2026-07-30 03:31:30 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const PRODUCTS = [
     usage: "Tomar 1 copa (30ml) al día, preferiblemente en la mañana.",
     price: 15600,
     originalPrice: 22300,
-    image: "assets/images/720-1280.mp4"
+    image: "assets/images/natural-medix.mp4"
   },
   {
     id: "origen-disco",
