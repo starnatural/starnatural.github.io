54412f29ed22f6eab2b49b8367e05f8139a44fa9
Star Natural <www.starnatural.co@gmail.com>
2026-07-31 23:39:10 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
    INICIALIZACIÓN DE LA APLICACIÓN (ENTRYPOINT)
    ========================================== */
 
-const APP_VERSION = "1.2.1";
+const APP_VERSION = "1.2.2";
 
 if (localStorage.getItem("app_version") !== APP_VERSION) {
   localStorage.setItem("app_version", APP_VERSION);
