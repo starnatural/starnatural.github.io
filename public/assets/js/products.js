2f84853a526cb6cc8299763dc69a54d4c8477309
Star Natural <www.starnatural.co@gmail.com>
2026-08-03 14:17:46 -0500

    Update products.js

--- public/assets/js/products.js
+++ public/assets/js/products.js
@@ -38,8 +38,8 @@ const PRODUCTS = [
     invima: "PSA-0005343-2024",
     benefit: "Alimento funcional con fibra natural que mejora la digestión y el tránsito intestinal.",
     usage: "Disolver 1 disco en un vaso de agua caliente al día.",
-    price: 17800,
-    originalPrice: 25450,
+    price: 143000,
+    originalPrice: 204300,
     image: "public/assets/images/origen_120_discos.mp4"
   },
    {
